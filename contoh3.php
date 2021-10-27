a4ea8daaac640dffd09b9b9dedb5ec148e142be7
ramadhan-17 <85573095+ramadhan-17@users.noreply.github.com>
2021-10-27 19:26:11 +0700

    Add files via upload

---
+++ contoh3.php
@@ -0,0 +1,11 @@
+<?php
+class contoh3 extends CI_Controller
+{
+    public function index()
+
+    {
+        echo"<h1>hallo</h1>";
+        echo"nama saya daffa
+        saya tinggal di tangerang";
+    }
+}
