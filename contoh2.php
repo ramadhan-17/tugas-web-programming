2b3b8bd0502bcbff9290abd3ddc36294bb6b4ea1
ramadhan-17 <85573095+ramadhan-17@users.noreply.github.com>
2021-10-27 19:25:59 +0700

    Add files via upload

---
+++ contoh2.php
@@ -0,0 +1,10 @@
+<?php
+class contoh2 extends CI_Controller
+{
+    public function index()
+
+    {
+        echo"<h1>HALLO SAYA HAFISH DAFFA RAMADHAN</h1>";
+        echo"HOBBY SAYA OALAHRAGA";
+    }
+}
